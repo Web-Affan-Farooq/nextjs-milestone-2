8ac67d0dd0e25217c69093116b79d905f04ba21b
Web-Affan-Farooq <affanamir903@gmail.com>
2024-11-10 15:57:05 +0500

    second commit

--- with-tailwind/src/app/layout.tsx
+++ with-tailwind/src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <body>
         <Header/>
         {children}
-        {/* <Footer/> */}
+        <Footer/>
       </body>
     </html>
   );
--- with-tailwind/src/app/page.tsx
+++ with-tailwind/src/app/page.tsx
@@ -6,8 +6,8 @@ import Section_2 from '../../components/Section-2/Section-2';
 const Home = () => {
   return (
     <div>
-      {/* <Section_1 /> */}
-      {/* <Section_2 /> */}
+      <Section_1 />
+      <Section_2 />
       <br />
       <br />
     </div>
